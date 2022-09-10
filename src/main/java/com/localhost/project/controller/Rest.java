2b71c3ff7b9d47f5c19c6bb2a81f1141696250c7
Antonio Rufino <devs.toni@gmail.com>
2022-09-10 22:10:40 +0200

    Añadido upload imagen de perfil con preview de subida
    Aun falta pulir detalles y refactorizar el código de la funcionalidad

--- src/main/java/com/localhost/project/controller/Rest.java
+++ src/main/java/com/localhost/project/controller/Rest.java
@@ -1,35 +1,44 @@
 package com.localhost.project.controller;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.google.gson.Gson;
 import com.localhost.project.entity.UserLogin;
 import com.localhost.project.service.UserService;
 import com.localhost.project.utils.FileUploadUtil;
 
-@Controller
-public class DataController {
-
+@RestController
+public class Rest {
+	
 	@Autowired
 	UserService userService;
 	@Autowired
 	FileUploadUtil fileUploadUtil;
 
-	@PostMapping("/upload-image")
-	public String uploadImage(@RequestParam("image") MultipartFile image, HttpServletRequest request) {
+	@PostMapping("/load")
+	public String loadPreview(@RequestParam(name = "image") MultipartFile image, HttpServletRequest request) {
+		String strImage = "";
+		Gson json = new Gson();
 		if (!image.getOriginalFilename().equals("")) {
-			String strImage = StringUtils.cleanPath(image.getOriginalFilename());
+			strImage = StringUtils.cleanPath(image.getOriginalFilename());
 			UserLogin user = userService.gerUserInSession(request);
-			userService.updateImageProfile(strImage, user.getId());
-			return "redirect:/home";
-		} else {
-			return "redirect:/home";
+			String upload = "src/main/resources/static/images/profile/" + user.getId();
+			try {
+				fileUploadUtil.save(upload, strImage, image);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
+		
+		return json.toJson(strImage);
 	}
 }
--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
-
 	$("#modal").modal('show')
 })
+
+async function sendPreview() {
+	let formData = new FormData();
+	formData.append("image", $('#fileupload').prop('files')[0])
+	let response = await fetch('/load', {
+		method: "POST",
+		body: formData
+	}).then((response) => response.json())
+		.then((name) => {
+			showPreview(name)
+		});
+	;
+
+}
+
+function showPreview(filename) {
+	$('#preview-photo').append(
+		$('<img/>')
+			.attr('id', 'preview')
+			.attr('src', 'images/profile/9/' + filename)
+			.addClass('preview')
+	);
+}
 
 
