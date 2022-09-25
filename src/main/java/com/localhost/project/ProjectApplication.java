3e7c2bca7c890711befa731e379d6e4b2e82bebf
Antonio Rufino <devs.toni@gmail.com>
2022-09-25 12:10:26 +0200

    front de componente card de publicaciones básico

--- src/main/java/com/localhost/project/ProjectApplication.java
+++ src/main/java/com/localhost/project/ProjectApplication.java
@@ -2,14 +2,18 @@ package com.localhost.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class ProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	    return builder.sources(ProjectApplication.class);
 	}
 }
