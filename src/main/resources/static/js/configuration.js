39622a2b052c84ca167bb05cc41f0b663e185161
Antonio Rufino <devs.toni@gmail.com>
2022-09-28 01:26:26 +0200

    menú de navegación de ajustes con estilos

--- src/main/resources/static/js/configuration.js
+++ src/main/resources/static/js/configuration.js
@@ -1,3 +1,22 @@
-/**
- * 
- */
+function show(e) {
+	if ($('#principal').attr('hidden')) {
+		$('#principal').removeAttr('hidden');
+	} else if (!$('#principal').attr('hidden')) {
+		$('#principal').attr('hidden', true);
+	}
+	$('#menu').toggleClass('site');
+
+	if (!$('#principal').attr('hidden')) {
+		switch (e.target.id) {
+			case "cuenta":
+				cuenta();
+				break;
+			case "profile":
+				break;
+		}
+	}
+}
+
+function cuenta() {
+	console.log("MOSTRANDO AJUSTES DE LA CUENTA")
+}
