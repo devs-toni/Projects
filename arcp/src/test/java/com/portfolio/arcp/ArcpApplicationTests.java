f17a34d5fdbe21fa5ba8f2141c455eeedde5a3d1
Antonio Rufino <devs.toni@gmail.com>
2022-11-12 00:38:37 +0100

    creation portfolio with react

---
+++ arcp/src/test/java/com/portfolio/arcp/ArcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.arcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
