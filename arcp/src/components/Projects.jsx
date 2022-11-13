0d1137c43c8f269250d2c765729556f20e13a767
Antonio Rufino <devs.toni@gmail.com>
2022-11-13 20:13:51 +0100

    projects structure finished

--- arcp/src/components/Projects.jsx
+++ arcp/src/components/Projects.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Project from './specific/Project';
+import arcp from '../assets/img/arcp.png';
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='courses projects'>
+      <Project img={arcp} title='Mi Portfolio' tech='developed with React.js'  />
+      <Project img='' title='Bikers' tech='Spring Boot'  />
+      <Project img='' title='Calculator' tech='React.js'  />
+      <Project img='' title='Pokemon Console' tech='Java'  />
+    </div>
   )
 }
 
