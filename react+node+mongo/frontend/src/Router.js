c55276c8dfcd0e19e0f8c5e1b987185ab4c54653
Antonio Rufino <devs.toni@gmail.com>
2022-11-20 03:28:53 +0100

    frontend header

---
+++ react+node+mongo/frontend/src/Router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
+import Header from './components/Header';
+
+const Router = () => {
+    return (
+        <Header />
+    );
+
+};
+
+export default Router;
---
+++ react+node+mongo/frontend/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import logo from '../assets/images/logo.svg';
+
+const Header = () => {
+  return (
+    <nav id='navbar' className='navbar navbar-expand-lg navbar-dark bg-dark'>
+      <div className='container'>
+        <NavLink to='/' className='navbar-brand'>
+          <img src={logo} className='App-logo' alt="logo" width='80' />
+        </NavLink>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink to='/articles' className='nav-link'>Artículos</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
